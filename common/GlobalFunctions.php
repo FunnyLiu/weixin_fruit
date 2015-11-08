701f00d7d91ff229adeccd126e6a466643ea0ae2
brizer <2281858473@qq.com>
2015-11-08 16:19:26 +0800

    Revert "添加日志管理函数"
    This reverts commit 54223dfc6c678b793a8d11ced858623fead6a4c4.

--- common/GlobalFunctions.php
+++ common/GlobalFunctions.php
@@ -3,7 +3,6 @@
  * 全局函数
  */
 require_once dirname(__FILE__) . '/GlobalDefine.php';
-require_once dirname(__FILE__) . 'MiniLog.php';
 //获取来源ip
 function getIp() {
 
@@ -78,71 +77,4 @@ function curlPost($url,$post){
 	$result = curl_exec($ch);
 	curl_close($ch);
 	return $result;
-}
-
-/**
- * 默认打开所有的日志文件
- * ERROR、INFO、DEBUG日志级别对应的关闭标记文件分别为NO_ERROR、NO_INFO、NO_DEBUG
- */
-function isLogLevelOff($logLevel) {
-  //ROOT_PATH在GlobalDefine.php中定义为当前文件的上一级目录
-  $switchFile = ROOT_PATH . '/log/' . 'NO_' . $logLevel;
-  if(file_exists($switchFile)) {
-    return true;
-  }else {
-    return false;
-  }
-}
-
-/**
- * @description 日志函数的入口
- * @param string $confName 日志配置名
- * @param string $logLevel 级别
- * @param int $errorCode 错误码
- * @param string $logMessage 日志内容
- */
-function wxmp_log($confName, $logLevel, $errorCode, $logMessage="no error msg") {
-  if(isLogLevelOff($logLevel)) {
-    return;
-  }
-  //产生一条 PHP 的回溯跟踪
-  $st = debug_backtrace();
-
-  $function = '';//调用wxmp_log的函数名
-  $file = '';//调用wxmp_log的文件名
-  $line = '';//调用wxmp_log的行号
-  foreach ($set as $item) {
-    if($file) {
-      $function = $item['function'];
-      break;
-    }
-    if($item['function'] == 'interface_log') {
-      $file = $item['file'];
-      $line = $item['line'];
-    }
-  }
-  $function = $function ? $function : 'main';
-
-  //为了缩短日志的输出，file只取最后一部分文件名
-  $file = explode('/', rtrim($file, '/'));
-  $file = $file[count($file)-1];
-  //组装日志的头部
-  $prefix = "[$file][$function][$line][$logLevel][$errorCode] ";
-  if($logLevel == INFO || $logLevel == STAT) {
-    $prefix = "[$logLevel]";
-  }
-  $logFileName = $confName . "_" . strtolower($logLevel);
-  MiniLog::instance(ROOT_PATH . "/log/")->log($logFileName, $prefix . $logMessage);
-  if(isLogLevelOff("DEBUG") || $logLevel == "DEBUG") {
-    return;
-  }else {
-    MiniLog::instance(ROOT_PATH . "/log/")->log($confName . "_" . "debug", $prefix . $logMessage);
-  }
-}
-
-/**
- * 实际用到的日志函数
- */
-function interface_log($logLevel, $errorCode, $logMessage = "no error msg") {
-  wxmp_log('interface', $logLevel, $errorCode, $logMessage);
 }
